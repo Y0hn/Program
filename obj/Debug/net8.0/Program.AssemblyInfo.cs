f94108b1c11d5ec1fb19066d48bfe595d5186093
Y0hn <janikanco@gmail.com>
2024-05-14 16:42:05 +0200

    Lin fix

--- obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc4c203a6aafcb0ae680f05517e44b90ce8d38e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bb6af18512b6f9c4c1d7428de2c817da4c4bbad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
