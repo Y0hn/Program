012df6206e983f02a26f8786d7d158ae44ec460b
John <101512527+Y0hn@users.noreply.github.com>
2024-06-03 22:28:04 +0200

    rerer

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
-﻿using Auta;
-
-namespace Program
+﻿namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
             ObsluznyProgram.Auta();
-            
         }
     }
 }
